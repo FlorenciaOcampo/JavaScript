eefdedd4c11cb19b355d05ab06b0bde3976294ba
unknown <florenciaaocampo1@gmail.com>
2022-07-11 15:14:34 -0300

    corrección Proyecto de Funciones

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-        function CalcularPromedio(){
-    let v1 = document.getElementById("n1").value;
-    let v2 = document.getElementById("n2").value;
-    let v3 = document.getElementById("n3").value;
-    let v4 = document.getElementById("n4").value;
-    let pro = (parseFloat(v1)+parseFloat(v2)+parseFloat(v3)+parseFloat(v4))/4;
-    document.getElementById("promedio").innerHTML = pro;
-}
+function promedio() {
+    let promedio = parseInt (prompt("Insertar promedio final"));
+    if (promedio > 6){
+        alert("Aprobado");
+    } 
+    else{
+        alert("Desaprobado. Derecho a Final 26/07");
+    }
+    }
+    promedio();
