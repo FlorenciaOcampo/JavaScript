c3177684f7b451299ad3cff8347a5d1ebd200bfe
unknown <florenciaaocampo1@gmail.com>
2022-07-10 22:04:59 -0300

    Primera Entrega de Proyecto

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-// while
-var mens = "cant. de act. de pág.";
-let contador = 0;
-while(contador <= 10){
-    alert(mens + " numero " + contador);
-    contador = contador + 1;
+        function CalcularPromedio(){
+    let v1 = document.getElementById("n1").value;
+    let v2 = document.getElementById("n2").value;
+    let v3 = document.getElementById("n3").value;
+    let v4 = document.getElementById("n4").value;
+    let pro = (parseFloat(v1)+parseFloat(v2)+parseFloat(v3)+parseFloat(v4))/4;
+    document.getElementById("promedio").innerHTML = pro;
 }
